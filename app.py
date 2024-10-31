8ac1db510eac472fa601560f984129443e269709
noe2019 <noe.fouotsa@aims-cameroon.org>
2024-10-30 20:51:22 -0400

    changed regression to classification

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from operator import index
 import streamlit as st
 import plotly.express as px
-from pycaret.regression import setup, compare_models, pull, save_model, load_model
+from pycaret.classification import setup, compare_models, pull, save_model, load_model
 import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
@@ -32,7 +32,7 @@ if choice == "Profiling":
 if choice == "Modelling": 
     chosen_target = st.selectbox('Choose the Target Column', df.columns)
     if st.button('Run Modelling'): 
-        setup(df, target=chosen_target, silent=True)
+        setup(df, target=chosen_target, verbose=False)
         setup_df = pull()
         st.dataframe(setup_df)
         best_model = compare_models()
